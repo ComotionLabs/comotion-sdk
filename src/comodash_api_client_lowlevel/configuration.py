826755d694c71f6c2227b264c1f77506ddcb6ed1
Tim Vieyra <664629+timalive@users.noreply.github.com>
2023-12-25 22:03:39 +0200

    Set debug to false

--- src/comodash_api_client_lowlevel/configuration.py
+++ src/comodash_api_client_lowlevel/configuration.py
@@ -121,7 +121,7 @@ class Configuration:
         self.logger_file = None
         """Debug file location
         """
-        self.debug = True
+        self.debug = False
         """Debug switch
         """
 
